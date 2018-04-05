7d4f31bdf64ee4464db3cabb5bffa65c500e1607
Nardberjean <Nardberjean@users.noreply.github.com>
2018-04-05 06:59:32 -0400

    Create install.bash

---
+++ install.bash
@@ -0,0 +1,40 @@
+#!/bin/sh
+# [[ ]] requires bash
+set -ev # https://docs.travis-ci.com/user/customizing-the-build/
+
+origin=$(pwd)
+composer create-project --no-install symfony/website-skeleton $CREATE_PROJECT_DIRECTORY
+if [[ -v $CREATE_PROJECT_DIRECTORY ]]; then
+  cd $CREATE_PROJECT_DIRECTORY
+else
+  cd website-skeleton
+fi
+composer config bin-dir bin
+# cp $origin/.env.dist . # Needs apparently to be done before install.
+composer install
+# install --directory config # Is it really needed?
+# composer require symfony/yaml # in symfony/website-skeleton
+# composer require symfony/console # in symfony/website-skeleton
+# composer require symfony/twig-bundle # in symfony/website-skeleton
+# composer require sensio/framework-extra-bundle # in symfony/website-skeleton
+# composer require symfony/orm-pack # in symfony/website-skeleton
+# composer require symfony/swiftmailer-bundle # in symfony/website-skeleton
+# composer require symfony/security-csrf
+# cp $origin/config/packages/*.yaml config/packages --verbose
+# cp $origin/config/routes/*.yaml config/routes --verbose
+# composer require friendsofsymfony/user-bundle
+
+# cp $origin/src/Entity/*.php src/Entity --verbose # May be done earlier.
+# bin/console doctrine:database:create
+# bin/console doctrine:migrations:diff --quiet
+# bin/console doctrine:migrations:migrate --no-interaction --quiet
+# bin/console doctrine:schema:update --force
+# composer require doctrine/doctrine-fixtures-bundle --dev
+# cp $origin/src/DataFixtures/AppFixtures.php src/DataFixtures
+# bin/console doctrine:fixtures:load --append
+# bin/console fos:user:create superadmin superadmin@example.com superadmin --super-admin
+# bin/console fos:user:create user user@example.com user
+
+cp --recursive $origin/tests . --verbose
+
+bin/console assets:install --symlink
